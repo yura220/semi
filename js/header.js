865e850208869a4e27e4925d51acefd95a867969
yura220 <ruddbfk7@gmail.com>
2025-07-17 16:14:30 +0900

    7/17

---
+++ js/header.js
@@ -0,0 +1,7 @@
+$(document).ready(
+    function(){
+        // $("header").css("background-color","red");
+        // $("header").css("top","0px");
+        $("header").css({"background-color":"#ccc","top":"0px"});
+    }
+);
